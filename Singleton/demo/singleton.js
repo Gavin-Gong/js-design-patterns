70d195dbe0911b075e2a91145b4c7ccf2946a2b3
Gavin-Gong <kefengong@gmail.com>
2017-03-06 09:14:05 +0800

    singlton pattern demo

--- Singleton/demo/singleton.js
+++ Singleton/demo/singleton.js
@@ -1,24 +1,47 @@
 let Dialog = (function() {
 	let instance = null;
+
 	function init() {
+		// 插入DOM元素
 		let maskEle = document.createElement('div');
 		maskEle.classList.add('show-dialog', 'dialog');
+		maskEle.style = 'display: block';
 		console.log(maskEle);
 		document.body.appendChild(maskEle);
-		return {
-			
-		}
+		
+		instance = {
+			$el: maskEle, // 模态元素
+			$isOpen: true, // dialog状态
+			close () {
+				this.$el.style = 'display: none';
+				this.$isOpen = false;
+			},
+			open () {
+				this.$el.style = 'display: block'
+				this.$isOpen = true;
+			},
+			toggle () {
+				if (this.$isOpen) {
+					this.close();
+				} else {
+					this.open();
+				}
+			}
+		};
 	}
 	function Dialog(isShow, title, content) {
 		if (!instance) {
 			init();
-			instance = true
 		}
+		return instance;
 	}
 	return Dialog;
 }());
 
-Dialog(true, 'title', 'content');
+// Dialog().open() 也可以这样调用
+let myDialog = new Dialog('title', 'content');
 
-Dialog.open('title', 'content');
-Dialog.close();
+document.getElementsByClassName('dialog-btn')[0].addEventListener('click', function() {
+	console.log('here');
+	myDialog.toggle();
+});
